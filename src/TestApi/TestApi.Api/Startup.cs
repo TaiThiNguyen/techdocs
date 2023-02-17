a690f5c7e8b034377d757aaf34f3030d0ae02a28
TaiThiNguyen <124208709+TaiThiNguyen@users.noreply.github.com>
2023-02-17 11:19:10 +0100

    TestApi

---
+++ src/TestApi/TestApi.Api/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Api.Config;
+
+public static class Startup
+{
+    public static IServiceCollection AddOpenTelemetry(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddAuthenticationConfiguration(this IServiceCollection services, ElvidConfig elvidConfig)
+    {
+        return services;
+    }
+}
+
